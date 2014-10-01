92dbfd15f37c5ec86b3d9b36c48a6022637113a1
Neha Kothari <sneha.kothari@gmail.com>
2014-10-01 16:18:47 -0700

    First revision

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterMensUSSizeTextField: UITextField!
+    
+    @IBOutlet weak var convertedMensShoeSizeLabel: UILabel!
+    
+    @IBOutlet weak var enterWomensUSSizeTextField: UITextField!
+    
+    @IBOutlet weak var convertedWomensShoeSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        enterMensUSSizeTextField.resignFirstResponder()
+        convertedMensShoeSizeLabel.hidden = false
+        let mensUSShoeSize = enterMensUSSizeTextField.text.toInt()!
+        var convertedMensShoeSize = mensUSShoeSize + 30
+        let convertedMensShoeSizeString = "\(convertedMensShoeSize)"
+        convertedMensShoeSizeLabel.text = "Equivalent European Men's Shoe Size Is \(convertedMensShoeSizeString)"
+    }
+    @IBAction func convertWomensButtonPressed(sender: UIButton) {
+        enterWomensUSSizeTextField.resignFirstResponder()
+        convertedWomensShoeSizeLabel.hidden = false
+        let convertedWomensShoesizeDouble = Double((enterWomensUSSizeTextField.text as NSString).doubleValue) + 30.5
+        convertedWomensShoeSizeLabel.text = "Equivalent European Women's Shoe Size is \(convertedWomensShoesizeDouble)"
+    }
 }
 
